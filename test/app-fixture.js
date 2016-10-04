15bfd709aa43d1d95b1308a00b4fc80008382153
helio-frota <00hf11@gmail.com>
2016-10-04 19:10:24 -0300

    Code comments.
    Initial idea of module usage and responsibility delegation.

--- test/app-fixture.js
+++ test/app-fixture.js
@@ -1,17 +1,25 @@
 'use strict';
 
-// =========================================================
+// ===========================================================
+// This need to be moved to hawkular-apm-client.
 const ip = require('ip');
+// This need to be moved to hawkular-apm-client.
 const lightbright = require('lightbright');
+// This need to be moved to hawkular-apm-client.
 const Timing = lightbright.builtins.timing;
+// This need to be moved to hawkular-apm-client.
 lightbright.addFilter(Timing.timer);
+// This need to be moved to hawkular-apm-client.
 lightbright.enable();
-// =========================================================
+// We need to use something like this:
+// const hawkular-apm-client = require('hawkular-apm-client');
+// ===========================================================
 
 const fs = require('fs');
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 
+// This need to be moved to hawkular-apm-client.
 function customTraces (t) {
   let traces = [];
   const hostAddress = ip.address();
@@ -45,9 +53,14 @@ server.route({
   path: '/hello',
   handler: function (request, reply) {
     // =========================================================
+    // This need to be moved to hawkular-apm-client.
     let t = Timing.timings();
+    // This need to be moved to hawkular-apm-client.
     console.log(customTraces(t));
     // lightbright.disable();
+
+    // We need to use something like this:
+    // hawkular-apm-client().publishTraces();
     // =========================================================
     return reply('Go to: <a href="http://localhost:8080/hawkular-ui/apm">http://localhost:8080/hawkular-ui/apm</a>');
   }
